5a15359029f98da87361b69330d05f7d77944289
Rafael Borba <rafaeborba@gmail.com>
2022-01-12 12:09:11 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,33 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing(){
   return(
-    <Pagination/>
+    <>
+      <Pagination/>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            {/* col-sm-6 define padroes de tamanho e o quanto ocupar, sm e um tamanho de tela assim como o lg/xl e o 6,4 e 3 define quanto ele vai ocupar sendo o maximo 12 */}
+            <MovieCard/>
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard/>
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard/>
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard/>
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard/>
+          </div>
+    
+        </div>
+      </div>
+    </>
   );
 }
 
