6c20f1a6c99a4b0155eee0c62481ac5ee80e3af4
Rafael Borba <rafaeborba@gmail.com>
2022-01-14 15:49:52 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -0,0 +1,71 @@
+package com.devsuperior.dsmovie.dto;
+
+import com.devsuperior.dsmovie.entities.Movie;
+
+public class MovieDTO {
+
+  private Long id;
+  private String title;
+  private Double score;
+  private Integer count;
+  private String image;
+
+  public MovieDTO(Long id, String title, Double score, Integer count, String image) {
+    this.setId(id);
+    this.setTitle(title);
+    this.setScore(score);
+    this.setCount(count);
+    this.setImage(image);
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public Integer getCount() {
+    return count;
+  }
+
+  public void setCount(Integer count) {
+    this.count = count;
+  }
+
+  public Double getScore() {
+    return score;
+  }
+
+  public void setScore(Double score) {
+    this.score = score;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public MovieDTO() {
+  }
+
+  public MovieDTO(Movie movie) {
+    this.setId(movie.getId());
+    this.setTitle(movie.getTitle());
+    this.setScore(movie.getScore());
+    this.setCount(movie.getCount());
+    this.setImage(movie.getImage());
+  }
+}
